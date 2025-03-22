1a25262dca3abb9906202a53ea8ab9c6384a9fc7
Swayam Verma <swayamverma1999@gmail.com>
2025-03-22 23:08:44 +0530

    Update prepaidinsert.java

--- prepaidinsert.java
+++ prepaidinsert.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class prepaidinsert{
     private static final String url="jdbc:mysql://localhost:3306/JDBC";
     private static final String username="root";
-    private static final String password="vermaswayam@225346";
+    private static final String password="***************";
     public static void main(String[] args) {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
